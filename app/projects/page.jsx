72c9b76cfc49a93c1620c93cb232c3c77e4dd09d
Fred <huertaserick@yahoo.ie>
2025-04-04 13:59:50 +0100

    Trying to make projects look better...

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -6,33 +6,35 @@ import KEYWORD_BG from "@/app/utils/keywordsColor.js";
 const Projects = () => {
   return (
     <FadeInSection>
-      <div className="container relative z-20 grid h-auto gap-10 p-10 m-auto to pt-26 xl:grid-cols-3 lg:grid-cols-2">
+      <div className="container relative z-20 grid h-auto gap-10 p-2 m-auto lg:p-10 to pt-26 xl:grid-cols-3 lg:grid-cols-2">
         {projects.map((project) => (
           <div
             key={project.title}
             className="flex flex-col p-3 space-y-3 bg-gray-700 border rounded border-slate-500"
           >
-            <div className="flex justify-center w-full">
+            <div className="flex justify-center lg:h-[300px] w-full">
               <Image
                 src={project.image}
-                className="object-contain border rounded lg:w-full md:w-82 bg-slate-800 border-slate-600"
-                width={500}
-                height={250}
+                className="object-contain border rounded lg:w-full bg-slate-800 border-slate-600"
+                width={1000}
+                height={500}
                 alt={project.title}
               />
             </div>
 
             <div className="flex flex-col flex-1 p-3">
               <h2 className="text-3xl font-bold">{project.title}</h2>
-              <p className="flex-1 py-5 text-slate-400">
+              <p className="flex-1 py-2 text-slate-400">
                 {project.description}
               </p>
             </div>
-            <div>
-              <ul className="flex flex-wrap gap-2">
+
+            <div className="p-5 border rounded border-slate-500 bg-slate-600">
+              <h2 className="">Tech Stack:</h2>
+              <ul className="flex flex-wrap gap-1">
                 {project.keywords.map((word, index) => (
                   <li
-                    className={`px-2 py-0.5 border rounded border-white text-black text-sm ${KEYWORD_BG(
+                    className={`px-3 text-sm italic py-0.5 border-2 rounded-lg border-slate-400 text-slate-900 ${KEYWORD_BG(
                       word
                     )}`}
                     key={index}
@@ -42,15 +44,13 @@ const Projects = () => {
                 ))}
               </ul>
             </div>
-            <button className="self-end px-2 py-5 transition-all ease-in-out hover:-translate-y-1">
-              <a
-                href={project.url}
-                className="p-2 rounded bg-sky-500 hover:bg-sky-600"
-                target="_blank"
-              >
-                Check it out
-              </a>
-            </button>
+            <a
+              href={project.url}
+              className="self-end px-5 py-2 mt-5 transition-all ease-in-out border rounded hover:-translate-y-1 border-slate-500 bg-sky-500 hover:bg-sky-600"
+              target="_blank"
+            >
+              Check it out
+            </a>
           </div>
         ))}
       </div>
