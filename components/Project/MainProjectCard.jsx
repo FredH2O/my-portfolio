87da9077b51e46c8f3077af1dcc52d34c36edd8c
Fred <huertaserick@yahoo.ie>
2025-04-11 18:47:33 +0100

    Remove fade in and out from main project card

--- components/Project/MainProjectCard.jsx
+++ components/Project/MainProjectCard.jsx
@@ -26,50 +26,48 @@ const MainProjectCard = () => {
       <p className="italic text-center">Click a project to view details.</p>
     );
   return (
-    <FadeInSection>
-      <div
-        onClick={closeProject}
-        className="fixed inset-0 z-50 flex h-auto overflow-y-auto bg-black/95"
-      >
-        <div className="container relative grid justify-center h-full gap-1 m-auto pt-15 lg:grid-cols-2 place-items-center">
-          <div className="px-1">
-            <Image
-              className="object-contain border rounded lg:w-full w-md border-slate-600"
-              src={selectedProject.image}
-              width={800}
-              height={800}
-              alt={selectedProject.title}
-            />
-          </div>
-
-          <div className="flex flex-col items-center justify-center p-3">
-            <h2 className="py-3 text-3xl font-bold">{selectedProject.title}</h2>
-            <p className="px-2 py-5 overflow-auto text-center text-slate-300 lg:text-left">
-              {selectedProject.details.split("\n").map((line, index) => (
-                <span key={index}>
-                  {line}
-                  <br />
-                </span>
-              ))}
-            </p>
-            <Keywords selectedProject={selectedProject} />
-            <a
-              href={selectedProject.url}
-              target="_blank"
-              className="self-end p-3 mt-2 rounded bg-sky-500 hover:bg-sky-600"
-            >
-              Live Demo
-            </a>
-          </div>
-
-          <X
-            onClick={closeProject}
-            size={35}
-            className="absolute bg-red-500 rounded cursor-pointer xl:top-40 xl:right-5 lg:top-20 top-2 right-2"
+    <div
+      onClick={closeProject}
+      className="fixed inset-0 z-50 flex h-auto overflow-y-auto bg-black/95"
+    >
+      <div className="container relative grid justify-center h-full gap-1 m-auto pt-15 lg:grid-cols-2 place-items-center">
+        <div className="px-1">
+          <Image
+            className="object-contain border rounded lg:w-full w-md border-slate-600"
+            src={selectedProject.image}
+            width={800}
+            height={800}
+            alt={selectedProject.title}
           />
         </div>
+
+        <div className="flex flex-col items-center justify-center p-3">
+          <h2 className="py-3 text-3xl font-bold">{selectedProject.title}</h2>
+          <p className="px-2 py-5 overflow-auto text-center text-slate-300 lg:text-left">
+            {selectedProject.details.split("\n").map((line, index) => (
+              <span key={index}>
+                {line}
+                <br />
+              </span>
+            ))}
+          </p>
+          <Keywords selectedProject={selectedProject} />
+          <a
+            href={selectedProject.url}
+            target="_blank"
+            className="self-end p-3 mt-2 rounded bg-sky-500 hover:bg-sky-600"
+          >
+            Live Demo
+          </a>
+        </div>
+
+        <X
+          onClick={closeProject}
+          size={35}
+          className="absolute bg-red-500 rounded cursor-pointer xl:top-40 xl:right-5 lg:top-20 top-2 right-2"
+        />
       </div>
-    </FadeInSection>
+    </div>
   );
 };
 
